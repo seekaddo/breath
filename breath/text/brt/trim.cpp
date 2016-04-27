b002f6b3c3ffb5adc95810f800513c479342c60f
Gennaro Prota <gennaro.prota+github@gmail.com>
2016-04-27 19:29:52 +0200

    Add a new facility: trim()

---
+++ breath/text/brt/trim.cpp
@@ -0,0 +1,36 @@
+// =========================================================================
+//                       Copyright 2016 Gennaro Prota
+//
+//                 Licensed under the BSD 3-Clause License.
+//            (See accompanying file BSD_3_CLAUSE_LICENSE.txt or
+//              <https://opensource.org/licenses/BSD-3-Clause>)
+// _________________________________________________________________________
+
+#include "breath/text/trim.hpp"
+#include "breath/text/set_of_chars.hpp"
+#include "breath/text/trim_head.hpp"
+#include "breath/text/trim_tail.hpp"
+
+
+namespace breath {
+
+std::string
+trim( std::string const & str )
+{
+    return breath::trim( str, set_of_chars(" \f\n\r\t\v") ) ;
+}
+
+std::string
+trim( std::string const & source, set_of_chars const & to_remove )
+{
+    return trim_head( trim_tail( source, to_remove ), to_remove ) ;
+}
+
+}
+
+// Local Variables:
+// mode: c++
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// End:
+// vim: set ft=cpp et sts=4 sw=4:
---
+++ breath/text/trim.hpp
@@ -0,0 +1,34 @@
+// =========================================================================
+//                       Copyright 2016 Gennaro Prota
+//
+//                 Licensed under the BSD 3-Clause License.
+//            (See accompanying file BSD_3_CLAUSE_LICENSE.txt or
+//              <https://opensource.org/licenses/BSD-3-Clause>)
+// _________________________________________________________________________
+
+#ifndef BREATH_GUARD_rws4wbvfhp1gTSnTPzZEhVe9JHM0Cwgk
+#define BREATH_GUARD_rws4wbvfhp1gTSnTPzZEhVe9JHM0Cwgk
+
+#include <string>
+
+namespace breath {
+
+class               set_of_chars ;
+
+std::string         trim( std::string const & str ) ;
+
+std::string         trim( std::string const &  str,
+                          set_of_chars const & to_remove ) ;
+
+}
+
+
+
+#endif
+
+// Local Variables:
+// mode: c++
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// End:
+// vim: set ft=cpp et sts=4 sw=4:
