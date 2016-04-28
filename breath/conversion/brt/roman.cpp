b0b0d7eb4840f169a7bea656c9d2a75d77df0128
Gennaro Prota <gennaro.prota+github@gmail.com>
2016-04-28 13:17:08 +0200

    Use nullptr in lieu of empty string literal

--- breath/conversion/brt/roman.cpp
+++ breath/conversion/brt/roman.cpp
@@ -25,7 +25,7 @@ roman::roman( int n )
         { 900, "CM" }, { 500, "D" }, { 400, "CD" }, { 100, "C" },
          { 90, "XC" },  { 50, "L" },  { 40, "XL" },  { 10, "X" },
           { 9, "IX" },   { 5, "V" },   { 4, "IV" },   { 1, "I" },
-          { 0, "" } 
+          { 0, nullptr } 
     } ;
 
     m_representation.reserve( 15 ) ;
