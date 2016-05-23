b0c73f5a6fc05de7455f498cd7eb16b55c7c855c
Gennaro Prota <gennaro.prota+github@gmail.com>
2016-05-23 20:10:55 +0200

    Fix some boilerplate

--- breath/diagnostics/brt/assert.cpp
+++ breath/diagnostics/brt/assert.cpp
@@ -1,10 +1,10 @@
-//////////////////////////////////////////////////////////////////////////////
-//                   Copyright (c) 2006-2010 Gennaro Prota
+// =========================================================================
+//                    Copyright 2006-2010 Gennaro Prota
 //
-//                 Licensed under the Simplified BSD License.
-//            (See accompanying file SIMPLIFIED_BSD_LICENSE.txt or
-//              <http://opensource.org/licenses/bsd-license.php>)//gps
-//  _________________________________________________________________________
+//                 Licensed under the BSD 3-Clause License.
+//            (See accompanying file BSD_3_CLAUSE_LICENSE.txt or
+//              <https://opensource.org/licenses/BSD-3-Clause>)
+// _________________________________________________________________________
 
 #include "breath/diagnostics/assert.hpp"
 
@@ -31,3 +31,10 @@ fire( char const * message, char const * file_name, unsigned long line_number )
 
 }
 }
+
+// Local Variables:
+// mode: c++
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// End:
+// vim: set ft=cpp et sts=4 sw=4:
