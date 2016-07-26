ebe831259c0472123df3e1d86a81579e81e64c8a
Gennaro Prota <gennaro.prota+github@gmail.com>
2016-07-26 20:41:57 +0200

    Add boilerplate text

--- breath/diagnostics/brt/dep/syst/unix/last_api_error.cpp
+++ breath/diagnostics/brt/dep/syst/unix/last_api_error.cpp
@@ -1,3 +1,11 @@
+// =========================================================================
+//                       Copyright 2015 Gennaro Prota
+//
+//                 Licensed under the BSD 3-Clause License.
+//            (See accompanying file BSD_3_CLAUSE_LICENSE.txt or
+//              <https://opensource.org/licenses/BSD-3-Clause>)
+// _________________________________________________________________________
+
 #include <string.h>
 #include <cerrno>
 #include <ostream>
@@ -45,3 +53,10 @@ operator<<( std::ostream & dest, last_api_error const & last)
 }
 
 }
+
+// Local Variables:
+// mode: c++
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// End:
+// vim: set ft=cpp et sts=4 sw=4:
--- breath/diagnostics/brt/dep/syst/windows/last_api_error.cpp
+++ breath/diagnostics/brt/dep/syst/windows/last_api_error.cpp
@@ -1,3 +1,11 @@
+// =========================================================================
+//                       Copyright 2015 Gennaro Prota
+//
+//                 Licensed under the BSD 3-Clause License.
+//            (See accompanying file BSD_3_CLAUSE_LICENSE.txt or
+//              <https://opensource.org/licenses/BSD-3-Clause>)
+// _________________________________________________________________________
+
 #include <Windows.h>
 #include <cstddef>
 #include <cstring>
@@ -70,3 +78,10 @@ operator<<( std::ostream & dest, last_api_error const & last )
 }
 
 }
+
+// Local Variables:
+// mode: c++
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// End:
+// vim: set ft=cpp et sts=4 sw=4:
