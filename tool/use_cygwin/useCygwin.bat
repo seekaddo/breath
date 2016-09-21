fc8854965af3cb5c6f39e616053b2e74ed77104b
Gennaro Prota <gennaro.prota+tortoisegit@gmail.com>
2016-09-21 08:24:00 +0200

    In tool/use_cygwin/useCygwin.bat, change some comments

--- tool/use_cygwin/useCygwin.bat
+++ tool/use_cygwin/useCygwin.bat
@@ -6,6 +6,7 @@ REM                 Licensed under the BSD 3-Clause License.
 REM            (See accompanying file BSD_3_CLAUSE_LICENSE.txt or
 REM              <https://opensource.org/licenses/BSD-3-Clause>)
 REM _________________________________________________________________________
+REM
 REM     A quick tool to invoke before compiling with the Cygwin tools,
 REM     on Windows.
 
@@ -13,8 +14,6 @@ REM     The only important thing that this does is putting C:\Cygwin\bin
 REM     at the *end* of %PATH%, so that Cygwin's `link' isn't found
 REM     before the homonyms Microsoft linker.
 REM
-REM     Created: October 14, 2013
-
 REM     Usage:
 REM         1. invoke the Visual Studio Command Prompt of your choice
 REM            (x86, x64, etc.)
