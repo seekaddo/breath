d75dc7844f75a585aceb1940c734ba329fb1dea2
Gennaro Prota <gennaro.prota+tortoisegit@gmail.com>
2016-09-30 13:07:02 +0200

    Fix compilation errors

--- breath/diagnostics/brt/dep/syst/windows/last_api_error.cpp
+++ breath/diagnostics/brt/dep/syst/windows/last_api_error.cpp
@@ -50,7 +50,7 @@ last_api_error::last_api_error( char const * p ) noexcept
 }
 
 last_api_error::last_api_error( last_api_error const & other ) noexcept
-    :   m_last_error( other.m_last_error )
+    :   exception(), m_last_error( other.m_last_error )
 {
     std::strcpy( &m_message[ 0 ], &other.m_message[ 0 ] ) ;
 }
@@ -59,7 +59,7 @@ last_api_error::~last_api_error() noexcept
 {
 }
 
-DWORD
+unsigned long
 last_api_error::code() const noexcept
 {
     return m_last_error ;
