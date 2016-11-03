1f7357b0c3d305e6ea3d759b41a4bfc0cbd4acce
Gennaro Prota <gennaro.prota+tortoisegit@gmail.com>
2016-11-03 16:50:37 +0100

    Adjust code formatting

--- breath/environment/windows_only/os_id.hpp
+++ breath/environment/windows_only/os_id.hpp
@@ -20,72 +20,71 @@
 
 namespace breath {
 
-    //      A thin wrapper around an int: we assign distinct integral
-    //      numbers to the different Windows versions.
-    //      ===============================================================
-    //
-    // Comparison operators rationale:
-    //
-    //   In my first idea of this class, it was my intent to support inequality
-    //   comparisons:
-    //
-    //     // windows 2000 or "later"?
-    //     if( id > os_id::windows_2000 )
-    //
-    //   But...
-    //   not only this might not make sense for other systems; at the time of
-    //   writing (April 2007) Windows versions seem to have an "official" total
-    //   ordering (see the various requires clauses in the SDK docs), but what
-    //   about the future? MS seems on the way of "parallelizing" their OS
-    //   products in a "client line" (Windows XP etc.) and a "server line"
-    //   (Windows Server 2003 etc.); this makes me think that we could have two
-    //   future Windows variants of which neither is "higher" or "lower" than
-    //   the other one
-    class os_id
-        : private equality_comparison< os_id > 
-    {
-    private:
-        typedef int     id_type ;
-        id_type         m_id ;
+//      A thin wrapper around an int: we assign distinct integral
+//      numbers to the different Windows versions.
+//      ===============================================================
+//
+// Comparison operators rationale:
+//
+//   In my first idea of this class, it was my intent to support inequality
+//   comparisons:
+//
+//     // windows 2000 or "later"?
+//     if( id > os_id::windows_2000 )
+//
+//   But...
+//   not only this might not make sense for other systems; at the time of
+//   writing (April 2007) Windows versions seem to have an "official" total
+//   ordering (see the various requires clauses in the SDK docs), but what
+//   about the future? MS seems on the way of "parallelizing" their OS
+//   products in a "client line" (Windows XP etc.) and a "server line"
+//   (Windows Server 2003 etc.); this makes me think that we could have two
+//   future Windows variants of which neither is "higher" or "lower" than
+//   the other one
+class os_id
+    : private equality_comparison< os_id > 
+{
+private:
+    typedef int     id_type ;
+    id_type         m_id ;
 
-        explicit        os_id( id_type ) ;
+    explicit        os_id( id_type ) ;
 
-        friend std::ostream &
-                        operator<<( std::ostream &, os_id const & ) ;
+    friend std::ostream &
+                    operator<<( std::ostream &, os_id const & ) ;
 
-    public:
-        // compiler-generated copy and destructor
+public:
+    // compiler-generated copy and destructor
 
-        bool is_equal( const os_id & ) const ;
+    bool            is_equal( os_id const & ) const ;
 
-        static const os_id
-            windows_unknown,
+    static const os_id
+        windows_unknown,
 
-            win32s,
+        win32s,
 
-            windows_95,
-            windows_98,
-            windows_me,
+        windows_95,
+        windows_98,
+        windows_me,
 
-            windows_nt, // unknown/3.51?? gps
-            windows_2000,
-            windows_xp,
-            windows_server_2003,
-            windows_home_server,
-            windows_storage_server_2003,
-            windows_server_2003_r2,
-            windows_xp_professional_x64_edition,
-            windows_vista,
-            windows_server_2008,
-            windows_server_2008_r2,
-            windows_7
-        ;
+        windows_nt, // unknown/3.51?? gps
+        windows_2000,
+        windows_xp,
+        windows_server_2003,
+        windows_home_server,
+        windows_storage_server_2003,
+        windows_server_2003_r2,
+        windows_xp_professional_x64_edition,
+        windows_vista,
+        windows_server_2008,
+        windows_server_2008_r2,
+        windows_7
+    ;
 
-    } ;
+} ;
 
-    // gps controlla se mettiamo una dichiarazione anche prima (vedi crypto/)
-    std::ostream &
-    operator<<( std::ostream & dest, os_id const & id ) ;
+// gps controlla se mettiamo una dichiarazione anche prima (vedi crypto/)
+std::ostream & operator<<( std::ostream & dest, os_id const & id ) ;
 }
 
 #endif
