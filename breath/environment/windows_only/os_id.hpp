0d06f2170e01ed7ecbfc9a9e228113a0e42927d4
Gennaro Prota <gennaro.prota+tortoisegit@gmail.com>
2016-11-09 11:57:43 +0100

    Update copyright notices in environment/windows_only/

--- breath/environment/windows_only/os_id.hpp
+++ breath/environment/windows_only/os_id.hpp
@@ -1,5 +1,5 @@
 // =========================================================================
-//                       Copyright 2007 Gennaro Prota
+//                    Copyright 2007-2016 Gennaro Prota
 //
 //                 Licensed under the BSD 3-Clause License.
 //            (See accompanying file BSD_3_CLAUSE_LICENSE.txt or
