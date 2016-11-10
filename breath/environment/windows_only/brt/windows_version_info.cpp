6bbd0f282a6793271c5407084ce23db1a6d8f1da
Gennaro Prota <gennaro.prota+tortoisegit@gmail.com>
2016-11-10 08:52:19 +0100

    Remove unreachable breaks in switch-case

--- breath/environment/windows_only/brt/windows_version_info.cpp
+++ breath/environment/windows_only/brt/windows_version_info.cpp
@@ -96,223 +96,151 @@ windows_version_info::edition() const
     switch( dw ) {
         case PRODUCT_UNDEFINED:
             return "<undefined>" ;
-            break ;
         case PRODUCT_ULTIMATE:
             return "Ultimate Edition" ;
-            break ;
         case PRODUCT_HOME_BASIC:
             return "Home Basic Edition" ;
-            break ;
         case PRODUCT_HOME_PREMIUM:
             return "Home Premium Edition" ;
-            break ;
         case PRODUCT_ENTERPRISE:
             return "Enterprise Edition" ;
-            break ;
         case PRODUCT_HOME_BASIC_N:
             return "Home Basic N" ;
-            break ;
         case PRODUCT_BUSINESS:
             return "Business Edition" ;
-            break ;
         case PRODUCT_STANDARD_SERVER:
             return "Standard Edition" ;
-            break ;
         case PRODUCT_DATACENTER_SERVER:
             return "Datacenter Edition" ;
-            break ;
         case PRODUCT_SMALLBUSINESS_SERVER:
             return "Small Business Server" ;
-            break ;
         case PRODUCT_ENTERPRISE_SERVER:
             return "Server Enterprise (full installation)" ;
-            break ;
         case PRODUCT_STARTER:
             return "Starter" ;
-            break ;
         case PRODUCT_DATACENTER_SERVER_CORE:
             return "Server Datacenter (core installation)" ;
-            break ;
         case PRODUCT_STANDARD_SERVER_CORE:
             return "Server Standard (core installation)" ;
-            break ;
         case PRODUCT_ENTERPRISE_SERVER_CORE:
             return "Server Enterprise (core installation)" ;
-            break ;
         case PRODUCT_ENTERPRISE_SERVER_IA64:
             return "Server Enterprise for Itanium-based Systems" ;
-            break ;
         case PRODUCT_BUSINESS_N:
             return "Business N" ;
-            break ;
         case PRODUCT_WEB_SERVER:
             return "Web Server (full installation)" ;
-            break ;
         case PRODUCT_CLUSTER_SERVER:
             return "HPC Edition" ;
-            break ;
         case PRODUCT_HOME_SERVER:
             return "Storage Server 2008 R2 Essentials" ;
-            break ;
         case PRODUCT_STORAGE_EXPRESS_SERVER:
             return "Storage Server Express" ;
-            break ;
         case PRODUCT_STORAGE_STANDARD_SERVER:
             return "Storage Server Standard" ;
-            break ;
         case PRODUCT_STORAGE_WORKGROUP_SERVER:
             return "Storage Server Workgroup" ;
-            break ;
         case PRODUCT_STORAGE_ENTERPRISE_SERVER:
             return "Storage Server Enterprise" ;
-            break ;
         case PRODUCT_SERVER_FOR_SMALLBUSINESS:
             return "for Windows Essential Server Solutions" ;
-            break ;
         case PRODUCT_SMALLBUSINESS_SERVER_PREMIUM:
             return "Small Business Server Premium" ;
-            break ;
         case PRODUCT_HOME_PREMIUM_N:
             return "Home Premium N" ;
-            break ;
         case PRODUCT_ENTERPRISE_N:
             return "Enterprise N" ;
-            break ;
         case PRODUCT_ULTIMATE_N:
             return "Ultimate N" ;
-            break ;
         case PRODUCT_WEB_SERVER_CORE:
             return "Web Server (core installation)" ;
-            break ;
         case PRODUCT_MEDIUMBUSINESS_SERVER_MANAGEMENT:
             return "Essential Business Server Management Server" ;
-            break ;
         case PRODUCT_MEDIUMBUSINESS_SERVER_SECURITY:
             return "Essential Business Server Security Server" ;
-            break ;
         case PRODUCT_MEDIUMBUSINESS_SERVER_MESSAGING:
             return "Essential Business Server Messaging Server" ;
-            break ;
         case PRODUCT_SERVER_FOUNDATION:
             return "Server Foundation" ;
-            break ;
         case PRODUCT_HOME_PREMIUM_SERVER:
             return "Home Server 2011" ;
-            break ;
         case PRODUCT_SERVER_FOR_SMALLBUSINESS_V:
             return "without Hyper-V for Windows Essential Server Solutions" ;
-            break ;
         case PRODUCT_STANDARD_SERVER_V:
             return "Server Standard without Hyper-V (full installation)" ;
-            break ;
         case PRODUCT_DATACENTER_SERVER_V:
             return "Server Datacenter without Hyper-V (full installation)" ;
-            break ;
         case PRODUCT_ENTERPRISE_SERVER_V:
             return "Server Enterprise without Hyper-V (full installation)" ;
-            break ;
         case PRODUCT_DATACENTER_SERVER_CORE_V:
             return "Server Datacenter without Hyper-V (core installation)" ;
-            break ;
         case PRODUCT_STANDARD_SERVER_CORE_V:
             return "" ;
-            break ;
         case PRODUCT_ENTERPRISE_SERVER_CORE_V:
             return"Server Enterprise without Hyper-V (core installation)" ;
         case PRODUCT_HYPERV:
             return "Microsoft Hyper-V Server" ;
-            break ;
         case PRODUCT_STORAGE_EXPRESS_SERVER_CORE:
             return "Storage Server Express (core installation)" ;
-            break ;
         case PRODUCT_STORAGE_STANDARD_SERVER_CORE:
             return "Storage Server Standard (core installation)" ;
-            break ;
         case PRODUCT_STORAGE_WORKGROUP_SERVER_CORE:
             return "Storage Server Workgroup (core installation)" ;
-            break ;
         case PRODUCT_STORAGE_ENTERPRISE_SERVER_CORE:
             return "Storage Server Enterprise (core installation)" ;
-            break ;
         case PRODUCT_STARTER_N:
             return "Starter N" ;
-            break ;
         case PRODUCT_PROFESSIONAL:
             return "Professional" ;
-            break ;
         case PRODUCT_PROFESSIONAL_N:
             return "Professional N" ;
-            break ;
         case PRODUCT_SB_SOLUTION_SERVER:
             return "Small Business Server 2011 Essentials" ;
-            break ;
         case PRODUCT_SERVER_FOR_SB_SOLUTIONS:
             return "Server For SB Solutions" ;
-            break ;
         case PRODUCT_STANDARD_SERVER_SOLUTIONS:
             return "Server Solutions Premium " ;
-            break ;
         case PRODUCT_STANDARD_SERVER_SOLUTIONS_CORE:
             return "Server Solutions Premium (core installation)" ;
-            break ;
         case PRODUCT_SB_SOLUTION_SERVER_EM:
             return "Server For SB Solutions EM" ;
-            break ;
         case PRODUCT_SERVER_FOR_SB_SOLUTIONS_EM:
             return "Server For SB Solutions EM" ;
-            break ;
         case PRODUCT_SOLUTION_EMBEDDEDSERVER:
             return "MultiPoint Server" ;
-            break ;
         case PRODUCT_SOLUTION_EMBEDDEDSERVER_CORE:
             return "undocumented [PRODUCT_SOLUTION_EMBEDDEDSERVER_CORE]" ; // gps
-            break ;
         case PRODUCT_SMALLBUSINESS_SERVER_PREMIUM_CORE:
             return "Small Business Server Premium (core installation)" ;
-            break ;
         case PRODUCT_ESSENTIALBUSINESS_SERVER_MGMT:
             return "Essential Server Solution Management" ;
-            break ;
         case PRODUCT_ESSENTIALBUSINESS_SERVER_ADDL:
             return "Essential Server Solution Additional" ;
-            break ;
         case PRODUCT_ESSENTIALBUSINESS_SERVER_MGMTSVC:
             return "Essential Server Solution Management SVC" ;
-            break ;
         case PRODUCT_ESSENTIALBUSINESS_SERVER_ADDLSVC:
             return " Essential Server Solution Additional SVC" ;
-            break ;
         case PRODUCT_CLUSTER_SERVER_V:
             return "Server Hyper Core V" ;
-            break ;
         case PRODUCT_EMBEDDED:
             return "undocumented [PRODUCT_EMBEDDED]" ; //gps
-            break ;
         case PRODUCT_STARTER_E:
             return "Starter E" ;
-            break ;
         case PRODUCT_HOME_BASIC_E:
             return "Home Basic E" ;
-            break ;
         case PRODUCT_HOME_PREMIUM_E:
             return "Home Premium E" ;
-            break ;
         case PRODUCT_PROFESSIONAL_E:
             return "Professional E  " ;
-            break ;
         case PRODUCT_ENTERPRISE_E:
             return "Enterprise E" ;
-            break ;
         case PRODUCT_ULTIMATE_E:
             return "Ultimate E" ;
-            break ;
         case PRODUCT_UNLICENSED:
             return "*unlicensed*" ;
-            break ;
 
         default:
             return "<unknown edition>" ;
-            break ;
     }
 }
 
