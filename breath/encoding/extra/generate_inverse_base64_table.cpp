98f136c3406e2d623a7c08bc55dc7664819f229d
Gennaro Prota <gennaro.prota+tortoisegit@gmail.com>
2016-12-09 15:32:33 +0100

    Add boilerplate text

--- breath/encoding/extra/generate_inverse_base64_table.cpp
+++ breath/encoding/extra/generate_inverse_base64_table.cpp
@@ -1,3 +1,11 @@
+// =========================================================================
+//                       Copyright 2016 Gennaro Prota
+//
+//                 Licensed under the BSD 3-Clause License.
+//            (See accompanying file BSD_3_CLAUSE_LICENSE.txt or
+//              <https://opensource.org/licenses/BSD-3-Clause>)
+// _________________________________________________________________________
+
 #include <vector>
 #include <iostream>
 #include <ostream>
@@ -34,3 +42,10 @@ main()
     }
     os << "\n} ;" << std::endl ;
 }
+
+// Local Variables:
+// mode: c++
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// End:
+// vim: set ft=cpp et sts=4 sw=4:
