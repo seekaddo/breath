69615e71ac0a7f808d9907869aaadbcf725e1674
Gennaro Prota <gennaro.prota+tortoisegit@gmail.com>
2017-02-10 12:59:27 +0100

    Adjust code formatting

--- breath/text/printable_string.hpp
+++ breath/text/printable_string.hpp
@@ -28,8 +28,8 @@ public:
 private:
 
     friend std::ostream &
-                        operator<<( std::ostream & dest,
-                                    printable_string const &) ;
+                    operator<<( std::ostream & dest,
+                                printable_string const &) ;
 
     maybe< std::string >
                         m_value ;
