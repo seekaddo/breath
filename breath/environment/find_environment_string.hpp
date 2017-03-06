7fae2ec070ff5c8332c9d3815b7707e1653a7c74
Gennaro Prota <gennaro.prota+tortoisegit@gmail.com>
2017-03-06 10:10:46 +0100

    Tweak the find_environment_string() documentation

--- breath/environment/find_environment_string.hpp
+++ breath/environment/find_environment_string.hpp
@@ -27,8 +27,8 @@
 
         - if the searched to string is not found \c getenv() returns a
           null pointer; that can easily lead to undefined behavior,
-          typically by passing that value to one of the constructors
-          of \c std::string which take a <code>const char *</code>
+          if that value is passed to one of the constructors of
+          \c std::string which take a <code>char const *</code>
 */
 
 #include "breath/idiom/string_argument.hpp"
