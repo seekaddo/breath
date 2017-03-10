7f544a9459dadb92fe493558c233d88172842f21
Gennaro Prota <gennaro.prota+tortoisegit@gmail.com>
2017-03-10 17:27:26 +0100

    Add a new facility: get_environment() for Unix

---
+++ breath/process/brt/dep/syst/unix/get_environment.cpp
@@ -0,0 +1,50 @@
+// =========================================================================
+//                       Copyright 2017 Gennaro Prota
+//
+//                 Licensed under the BSD 3-Clause License.
+//            (See accompanying file BSD_3_CLAUSE_LICENSE.txt or
+//             <https://opensource.org/licenses/BSD-3-Clause>.)
+// _________________________________________________________________________
+
+#include "breath/process/get_environment.hpp"
+#include "breath/diagnostics/last_api_error.hpp"
+#include <unistd.h>
+
+namespace breath {
+
+std::map< std::string, std::string >
+get_environment()
+{
+    typedef std::map< std::string, std::string >
+                        result_type ;
+    if ( environ == nullptr ) {
+        throw last_api_error( "environ is a null pointer" ) ;
+    }
+
+    result_type         result ;
+
+    char const * const *
+                        curr = environ ;
+    while ( *curr != '\0' ) {
+        std::string const   single = *curr ;
+        auto const          pos = single.find( '=' ) ;
+        std::string const   name = single.substr( 0, pos ) ;
+
+        result.insert( result_type::value_type(
+                            name,
+                            single.substr( pos + 1 )
+                        ) ) ;
+
+        ++ curr;
+    }
+    return result ;
+}
+
+}
+
+// Local Variables:
+// mode: c++
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// End:
+// vim: set ft=cpp et sts=4 sw=4:
