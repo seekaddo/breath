d31b03b16c32abe2b708ec2f1319cc2b52736c5c
Gennaro Prota <gennaro.prota+tortoisegit@gmail.com>
2017-04-10 12:02:13 +0200

    Add two missing spaces

--- breath/random/brt/dep/syst/windows/entropy_source.cpp
+++ breath/random/brt/dep/syst/windows/entropy_source.cpp
@@ -52,7 +52,7 @@ impl()
     // note: m_handle_is_valid intentionally not initialized here --see below
 {
     if ( ! acquire() ) {
-        entropy_source::exception::raise("cannot acquire the HCRYPTPROV handler") ;
+        entropy_source::exception::raise( "cannot acquire the HCRYPTPROV handler" ) ;
     }
     // note that this member is intentionally not initialized in the ctor
     // initializer list; in case of exceptions there is no way it could be used
