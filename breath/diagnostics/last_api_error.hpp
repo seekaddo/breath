93d5733380cadc8c18997ea711ecb3a065a08281
Gennaro Prota <gennaro.prota+github@gmail.com>
2017-04-16 22:37:17 +0200

    Put last_api_error's general docs in common

--- breath/diagnostics/last_api_error.hpp
+++ breath/diagnostics/last_api_error.hpp
@@ -8,6 +8,20 @@
 //
 //!     \file
 //!     \brief The last error from the OS API.
+//!
+//!     An exception class representing the last error encountered
+//!     in an OS API call. It corresponds to GetLastError() under
+//!     Windows and errno under Unix.
+//!
+//!     The idea is that after an unsuccessful API call you write:
+//!     <code>
+//!         throw last_api_error( "<APIName>() failed" ) ;
+//!     </code>
+//!     and you're done with error handling.
+//!
+//!     The class is OutputStreamable, so you can also use
+//!     \c breath::as_string(), which gives more information than the
+//!     member \c what(), but may throw.
 // -------------------------------------------------------------------------
 
 #include "breath/diagnostics/private/last_api_error.hpp"
