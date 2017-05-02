7e80f1d98f4cd4160d71c6bb6becadd5f3cb878c
Gennaro Prota <gennaro.prota+github@gmail.com>
2017-05-02 11:03:48 +0200

    Remove a spurious space

--- breath/environment/brt/dep/syst/unix/operating_system_name.cpp
+++ breath/environment/brt/dep/syst/unix/operating_system_name.cpp
@@ -29,7 +29,7 @@ operating_system_name_error::what() const noexcept
 std::ostream &
 operator <<( std::ostream & dest, operating_system_name const & )
 {
-    utsname             un  ;
+    utsname             un ;
     int const           ret = uname( &un ) ;
 
     if ( ret == -1 ) {
