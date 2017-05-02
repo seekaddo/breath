98ed962029b9ccff69d69258780c5d6ce60c839e
Gennaro Prota <gennaro.prota+github@gmail.com>
2017-05-02 16:31:06 +0200

    Adjust code alignment

--- breath/random/subrange_adaptor.hpp
+++ breath/random/subrange_adaptor.hpp
@@ -30,7 +30,7 @@ public:
     typedef typename Engine::result_type
                         result_type ;
 
-    void operator =(subrange_adaptor const & ) = delete ; //gps get away with VC++'s C4512, for now
+    void                operator =(subrange_adaptor const & ) = delete ; //gps get away with VC++'s C4512, for now
 
     //!     Constructs a subrange adaptor from a given Engine and a new max
     //!     value.
