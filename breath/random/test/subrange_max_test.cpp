89472eaff7b43ee4d1b473ade6c34a84b721bb4b
Gennaro Prota <gennaro.prota+github@gmail.com>
2017-05-02 16:11:28 +0200

    Add unit tests for subrange_max()

---
+++ breath/random/test/subrange_max_test.cpp
@@ -0,0 +1,43 @@
+// =========================================================================
+//                       Copyright 2017 Gennaro Prota
+//
+//                 Licensed under the 3-Clause BSD License.
+//            (See accompanying file 3_CLAUSE_BSD_LICENSE.txt or
+//             <https://opensource.org/licenses/BSD-3-Clause>.)
+// _________________________________________________________________________
+
+
+// gps temp
+#undef NDEBUG
+#include <assert.h>
+#define DO_TEST( cond ) assert( (cond) )
+
+//----------------------
+
+#include "breath/random/subrange_max.hpp"
+
+int
+main()
+{
+    static struct {
+        int m ;
+        int x ;
+        unsigned int result ;
+    }                   values[] = {
+        { 5, 32767, 32765 },
+        { 5,   255,   251 },
+        { 1,   255,   255 },
+        { 0,   255,   255 }
+    
+    };
+    for ( auto elem : values ) {
+        DO_TEST( breath::subrange_max( elem.m, elem.x ) == elem.result ) ;
+    }
+}
+
+// Local Variables:
+// mode: c++
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// End:
+// vim: set ft=cpp et sts=4 sw=4:
