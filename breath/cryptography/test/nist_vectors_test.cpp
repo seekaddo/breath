cbbef8aad66c8a8c449835b80a608e2433450a5f
Gennaro Prota <gennaro.prota+github@gmail.com>
2017-05-05 11:10:56 +0200

    Apply our latest coding conventions to nist_vectors_test.cpp

--- breath/cryptography/test/nist_vectors_test.cpp
+++ breath/cryptography/test/nist_vectors_test.cpp
@@ -8,8 +8,9 @@
 //
 //      Tests the SHA-1 implementation with the test vectors
 //      described at <http://www.nsrl.nist.gov/testdata/>.
-//
+// -------------------------------------------------------------------------
 
+#include "breath/counting/count.hpp"
 #include "breath/cryptography/digest.hpp"
 #include "breath/cryptography/sha1_hasher.hpp"
 #include "breath/diagnostics/assert.hpp"
@@ -18,6 +19,7 @@
 #include "breath/text/to_string.hpp"
 
 #include <algorithm>
+#include <cstddef>
 #include <fstream>
 #include <iostream>
 #include <istream>
@@ -27,242 +29,247 @@
 
 namespace {
 
-    class nist_file
-    {
-        std::ifstream m_stream;
-
-        template< typename T >
-        friend nist_file & operator >>( nist_file &, T & t );
-
-    public:
-        explicit nist_file( const char * filename );
-        bool good() const;
-        bool new_section();
-
-    };
-
-    nist_file::nist_file( const char * filename )
-    {
-        std::string const
-                        breath_root( breath::find_environment_string( "BREATH_ROOT" ).value() ) ;
-        std::string const
-                        subdir( breath_root + "/breath/cryptography/test/nist_vectors/" );
-        m_stream.open( ( subdir + filename ).c_str() );
-        if ( m_stream )
-        {
-            for ( std::string s; s != "D>"; ) {
-                m_stream >> s;
-            }
-        }
-
-        if ( m_stream.rdstate() & ( std::ios::failbit | std::ios::eofbit ) )
-            throw std::ios::failure( "cannot construct nist_file object" );
-
-    }
-
-    bool nist_file::good() const
-    {
-        return m_stream.good();
-    }
-
-    bool nist_file::new_section()
-    {
-        for ( std::string s; s != "D>" && m_stream; ) {
-            m_stream >> s;
-        }
-
-        return m_stream.good();
-    }
+class nist_file
+{
+    std::ifstream       m_stream ;
 
     template< typename T >
-    nist_file & operator >>( nist_file & f, T & t )
+    friend nist_file &  operator >>( nist_file &, T & t ) ;
+
+public:
+    explicit            nist_file( const char * filename ) ;
+    bool                good() const ;
+    bool                new_section() ;
+} ;
+
+nist_file::nist_file( char const * filename )
+{
+    std::string const   breath_root( breath::find_environment_string(
+                                                    "BREATH_ROOT" ).value() ) ;
+    std::string const
+                        subdir( breath_root +
+                                    "/breath/cryptography/test/nist_vectors/" ) ;
+    m_stream.open( ( subdir + filename ).c_str() ) ;
+    if ( m_stream )
     {
-        f.m_stream >> t;
-        return f;
+        for ( std::string s ; s != "D>" ; ) {
+            m_stream >> s;
+        }
     }
 
-    class montecarlo_test
-    {
-        breath::sha1_hasher m;
-        int m_count;
-        typedef breath::sha1_engine::word_type word_type;
-        typedef breath::sha1_engine::byte_type byte_type; // gps esporre dall'hasher?
-    public:
-        montecarlo_test() : m_count( 0 )
-        {}
-
-        int get_count() const
-        {
-            return m_count;
-        }
-
-        void init( const std::string & seed ) // gps come la chiamo (vedi rnd)
-        {
-            m.append( seed.begin(), seed.end() );
-        }
-
-        breath::sha1_digest next()
-        {
-            breath::sha1_digest result( m /* bogus argument */ );
-            const word_type tot( 50 * 1000 );
-            for ( word_type i( 1 ); i <= tot; ++i )
-            {
-                for ( byte_type a( 1 ); a <= m_count / 4 + 3; ++a )
-                    m.push_back( '\0' );
-
-                m.push_back( static_cast< char >( ( i >> 24 ) & 0xff ) );
-                m.push_back( static_cast< char >( ( i >> 16 ) & 0xff ) );
-                m.push_back( static_cast< char >( ( i >>  8 ) & 0xff ) );
-                m.push_back( static_cast< char >( ( i       ) & 0xff ) );
-
-                // our "moral equivalent" of m = SHA1( m )
-                result = breath::sha1_digest( m );
-                m = breath::sha1_hasher( result.begin(), result.end() );
-
-            }
-
-            ++m_count;
-            return result;
-        }
-
-    };
-
-    std::string read_compact_string( nist_file & messages, int z )
-    {
-        bool b;
-        messages >> b;
-
-        std::string msg;
-        char curr( 0 );
-        unsigned mask = 128; // gps
-        for ( int i = 0; i < z; ++i )
-        {
-            unsigned n;
-            messages >> n;
-
-            while( n-- > 0 )
-            {
-                if ( b )
-                    curr |= mask;
-
-                mask /= 2;
-                if ( mask == 0 )
-                    msg += curr, curr = 0, mask = 128;
-            }
-
-            b = !b;
-        }
-
-        return msg;
-     }
-
+    if ( m_stream.rdstate() & ( std::ios::failbit | std::ios::eofbit ) )
+        throw std::ios::failure( "cannot construct nist_file object" ) ;
 }
 
-void tests()
+bool
+nist_file::good() const
 {
-    using namespace breath;
+    return m_stream.good() ;
+}
 
-    nist_file messages( "byte-messages.sha1" );
-    nist_file hashes  ( "byte-hashes.sha1" );
+bool
+nist_file::new_section()
+{
+    for ( std::string s ; s != "D>" && m_stream ; ) {
+        m_stream >> s ;
+    }
 
-    int total = 0;
-    int failed = 0;
+    return m_stream.good() ;
+}
 
-    static const struct { int section; bool pseudorandom; } section_types[]
+template< typename T >
+nist_file &
+operator >>( nist_file & f, T & t )
+{
+    f.m_stream >> t ;
+    return f ;
+}
+
+class montecarlo_test
+{
+    breath::sha1_hasher m ;
+    int                 m_count ;
+    typedef breath::sha1_engine::word_type
+                        word_type ;
+    typedef breath::sha1_engine::byte_type
+                        byte_type ; // gps esporre dall'hasher?
+public:
+                        montecarlo_test() : m_count( 0 )
+                        {
+                        }
+
+    int                 get_count() const
+    {
+        return m_count ;
+    }
+
+    void                init( const std::string & seed ) // gps come la chiamo (vedi rnd)
+    {
+        m.append( seed.cbegin(), seed.cend() ) ;
+    }
+
+    breath::sha1_digest next()
+    {
+        breath::sha1_digest result( m /* bogus argument */ ) ;
+        const word_type     tot( 50 * 1000 ) ;
+        for ( word_type i = 1 ; i <= tot ; ++ i )
+        {
+            for ( byte_type a( 1 ) ; a <= m_count / 4 + 3 ; ++ a )
+                m.push_back( '\0' ) ;
+
+            m.push_back( static_cast< char >( ( i >> 24 ) & 0xff ) ) ;
+            m.push_back( static_cast< char >( ( i >> 16 ) & 0xff ) ) ;
+            m.push_back( static_cast< char >( ( i >>  8 ) & 0xff ) ) ;
+            m.push_back( static_cast< char >( ( i       ) & 0xff ) ) ;
+
+            // our "moral equivalent" of m = SHA1( m )
+            result = breath::sha1_digest( m ) ;
+            m = breath::sha1_hasher( result.begin(), result.end() ) ;
+        }
+
+        ++ m_count ;
+        return result ;
+    }
+};
+
+std::string
+read_compact_string( nist_file & messages, int z )
+{
+    bool                b ;
+    messages >> b ;
+
+    std::string         msg ;
+    char                curr = '\0' ;
+    unsigned            mask = 128; // gps
+    for ( int i = 0 ; i < z ; ++ i ) {
+        unsigned            n ;
+        messages >> n ;
+
+        while( n-- > 0 ) {
+            if ( b )
+                curr |= mask ;
+
+            mask /= 2 ;
+            if ( mask == 0 )
+                msg += curr, curr = 0, mask = 128 ;
+        }
+
+        b = !b ;
+    }
+
+    return msg ;
+    }
+}
+
+void
+tests()
+{
+    using namespace breath ;
+
+    nist_file           messages( "byte-messages.sha1" ) ;
+    nist_file           hashes  ( "byte-hashes.sha1" ) ;
+
+    int                 total = 0 ;
+    int                 failed = 0 ;
+
+    static struct
+    {
+        int                 section ;
+        bool                pseudorandom ;
+    } const             section_types[]
     = {
         { 0, false },
         { 1, false },
         { 2, true  }
-    };
+    } ;
 
-    const int sections( sizeof section_types / sizeof section_types[ 0 ] );
+    std::size_t const   sections = breath::count( section_types ) ;
 
-    std::string calculated;
-    std::string msg;
-    montecarlo_test montecarlo_harness;
+    std::string         calculated ;
+    std::string         msg ;
+    montecarlo_test     montecarlo_harness ;
 
-    for ( int sn( 0 ); sn < sections && hashes.good() ; /*++sn*/ )
+    for ( std::size_t sn = 0 ; sn < sections && hashes.good() ; /*++sn*/ )
     {
-        bool montecarlo_section( section_types[ sn ].pseudorandom );
+        bool                montecarlo_section( section_types[ sn ].pseudorandom ) ;
         if ( /*!montecarlo_section ||*/ montecarlo_harness.get_count() == 0 )
         {
             // terminator found?
             //
-            std::string s;
-            messages >> s;
+            std::string         s ;
+            messages >> s ;
             if ( s == "<D" )
             {
-                messages.new_section();
-                hashes.new_section();
-                ++sn;
-                continue;
+                messages.new_section() ;
+                hashes.new_section() ;
+                ++sn ;
+                continue ;
             }
 
-            int z( from_string< int >( s ) );
+            int                 z( from_string< int >( s ) ) ;
 
             if ( montecarlo_section )
             {
                 if ( montecarlo_harness.get_count() == 0 ) {
 
-                    msg = read_compact_string( messages, z );
-                    BREATH_ASSERT( 8 * msg.length() == 416 );
+                    msg = read_compact_string( messages, z ) ;
+                    BREATH_ASSERT( 8 * msg.length() == 416 ) ;
 
-                    montecarlo_harness.init( msg );
+                    montecarlo_harness.init( msg ) ;
                 }
             }
             else
             {
-                msg = read_compact_string( messages, z );
+                msg = read_compact_string( messages, z ) ;
             }
 
-            std::string dummy;
-            messages >> dummy; // skip terminator
+            std::string         dummy ;
+            messages >> dummy ; // skip terminator
         }
 
-
-        std::string expected;
-        hashes >> expected;
+        std::string     expected ;
+        hashes >> expected ;
         if ( expected == "<D" )
-            break; // done!
+            break ; // done!
 
-        std::string unused;
-        hashes >> unused; // terminator
+        std::string     unused ;
+        hashes >> unused ; // terminator
 
         if ( hashes.good() )
-            ++total;
+            ++ total ;
 
         calculated = to_string(
-            montecarlo_section?
-                montecarlo_harness.next()
+            montecarlo_section
+              ? montecarlo_harness.next()
               : make_digest( sha1_hasher( msg.begin(), msg.end() ) )
-            );
+            ) ;
 
         if ( calculated != expected )
         {
             ++failed;
-            std::clog << "Failure on test vector # " << total << '\n';
-            std::clog << "\tCalculated: " << calculated << '\n';
-            std::clog << "\tExpected:   " << expected   << '\n';
+            std::clog << "Failure on test vector # " << total << std::endl ;
+            std::clog << "\tCalculated: " << calculated << std::endl ;
+            std::clog << "\tExpected:   " << expected   << std::endl ;
         }
-
     }
 
     // report results
     //
     std::cout << "\n\nTotal:  " << total  << " (of which "
               << montecarlo_harness.get_count() << " pseudorandom)" << '\n'
-              <<     "Failed: " << failed << std::endl;
+              <<     "Failed: " << failed << std::endl ;
 
 }
 
-int main()
+int
+main()
 try
 {
-    tests();
+    tests() ;
 }
-catch( std::exception & ex )
+catch( std::exception const & ex )
 {
-    std::cout << "Error: '" << ex.what() << '\'';
+    std::cout << "Error: '" << ex.what() << '\'' << std::endl ;
 }
 
 // Local Variables:
