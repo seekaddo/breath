b5a81a5df975d51d3e49d1b95ed1bee54500cb07
Gennaro Prota <gennaro.prota+github@gmail.com>
2017-06-09 21:52:00 +0200

    Add a couple of braces, as per our coding style

--- breath/cryptography/brt/hashing_count.cpp
+++ breath/cryptography/brt/hashing_count.cpp
@@ -18,9 +18,10 @@ hashing_count::exception::exception( char const * message )
 void
 hashing_count::on_length_exhausted( hashing_count::type t )
 {
-    if ( t == hashing_count::is_limited )
+    if ( t == hashing_count::is_limited ) {
         throw hashing_count::exception(
                 "input too long for this hash function" ) ;
+    }
 }
 
 }
