99c9a099d3cd4fb474212d05c9c5a4d0d8dd065d
Gennaro Prota <gennaro.prota+github@gmail.com>
2017-06-13 22:29:01 +0200

    Add is_power_of_two_test.cpp

---
+++ breath/mathematics/test/is_power_of_two_test.cpp
@@ -0,0 +1,49 @@
+// =========================================================================
+//                       Copyright 2017 Gennaro Prota
+//
+//                 Licensed under the 3-Clause BSD License.
+//            (See accompanying file 3_CLAUSE_BSD_LICENSE.txt or
+//             <https://opensource.org/licenses/BSD-3-Clause>.)
+// _________________________________________________________________________
+
+#include "breath/mathematics/is_power_of_two.hpp"
+#include "breath/testing/testing.hpp"
+#include <cstdint>
+#include <iostream>
+
+namespace {
+
+void
+do_check()
+{
+    BREATH_CHECK( ! breath::is_power_of_two( 0u ) ) ;
+    BREATH_CHECK( breath::is_power_of_two( 1u ) ) ;
+    for ( std::uint32_t  i = 2; i > 0 ; i *= 2 ) {
+        BREATH_CHECK( breath::is_power_of_two( i ) ) ;
+        if (i != 2) {
+            BREATH_CHECK( ! breath::is_power_of_two( i - 1 ) );
+        }
+        BREATH_CHECK( ! breath::is_power_of_two( i + 1 ) ) ;
+    }
+    BREATH_CHECK( breath::is_power_of_two( true ) ) ;
+}
+
+}
+
+int
+main()
+{
+    using namespace breath ;
+
+    console_reporter    cr( std::cout ) ;
+    test_runner::instance().attach_reporter( cr ) ;
+
+    return test_runner::instance().run( { do_check } ) ;
+}
+
+// Local Variables:
+// mode: c++
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// End:
+// vim: set ft=cpp et sts=4 sw=4:
