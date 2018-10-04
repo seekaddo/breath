3a6f4a531525fb4a92884819ecc59f0c6e00566c
Gennaro Prota <gennaro.prota+github@gmail.com>
2018-10-04 11:02:49 +0200

    Make progress on settling the Clang makefile
    More -Wno- options will be needed to compile successfully.

--- build/clang.mk
+++ build/clang.mk
@@ -1,24 +1,31 @@
 # =========================================================================
-#                       Copyright 2013 Gennaro Prota
+#                    Copyright 2013-2018 Gennaro Prota
 #
 #                 Licensed under the 3-Clause BSD License.
 #            (See accompanying file 3_CLAUSE_BSD_LICENSE.txt or
 #             <https://opensource.org/licenses/BSD-3-Clause>.)
 # _________________________________________________________________________
 
-# NOTE: for the moment (or not?), include gcc.mk then
-#       override what needs to be overridden.  --gps
+#       As usual, the options are listed in two groups. The group
+#       listed first relates mainly to standards conformance. The other
+#       group contains other options in alphabetical order.
 #
-include $(root)/build/gcc.mk
+#       Special notes for some of the options:
+#       ======================================
+#
+#       (See gcc.mk)
+# ----------------------------------------------------------------------------
+cpp_basic_options =  -std=c++14                 \
+                     -Weverything               \
+                     -Wno-c++98-compat          \
+                     -pedantic                  \
+                     -Werror                    \
+                     -D_POSIX_C_SOURCE=200112L  \
+                     -U_GNU_SOURCE              \
+                     -D_GLIBCXX_USE_CXX11_ABI
 
-cpp_basic_options = -std=c++14       \
-                    -Wall            \
-                    -Wextra          \
-                    -Werror          \
-                    -pedantic-errors \
-            -D_POSIX_C_SOURCE=200112L
+cpp_basic_options += -pipe
 
-cpp_extra_options = -pipe	\
 # gps queste vanno nel debug:
 #cpp_extra_options += -fcatch-undefined-behavior
 
@@ -26,11 +33,25 @@ cpp_extra_options = -pipe	\
 #                    -verify     #OK?? gps
 
 
-cpp_extra_options += -isystem"/usr/lib/gcc/i686-pc-cygwin/4.7.3/include/c++"  ### ^^^^^gps TEMP!!!!
-cpp_extra_options += -isystem"/usr/lib/gcc/i686-pc-cygwin/4.7.3/include/c++/i686-pc-cygwin" ## gps temp
+include_switch = -I
+object_file_suffix = .o
 
-
+# Note that this differs from the compiler name, which is in $(compiler)
+#
 compiler_command = clang++
+
+# gps mostly by James!
+define compile_to_object
+# gps controlla la riga seguente!!!
+$(compiler_command) $(cpp_options) -c -o $@ $<
+endef
+
+# gps mostly by James!
+#
+define link_to_exec
+$(compiler_command) $(cpp_options) -o $@ $+
+endef
+
 
 # Local Variables:
 # mode: makefile
