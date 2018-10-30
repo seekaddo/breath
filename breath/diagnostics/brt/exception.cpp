591d5611e404430c51eaa7cf67b3e701d74fc66e
Gennaro Prota <gennaro.prota+github@gmail.com>
2018-10-30 10:01:41 +0100

    Always terminate the buffer embedded in breath::exception
    It wasn't terminated if the passed string was very long (precisely, if
    what_msg was so long that the count given to std::strncpy() was reached
    before reaching the terminator in what_msg.c_str()).

--- breath/diagnostics/brt/exception.cpp
+++ breath/diagnostics/brt/exception.cpp
@@ -1,5 +1,5 @@
 // =========================================================================
-//                       Copyright 2015 Gennaro Prota
+//                    Copyright 2015-2018 Gennaro Prota
 //
 //                 Licensed under the 3-Clause BSD License.
 //            (See accompanying file 3_CLAUSE_BSD_LICENSE.txt or
@@ -8,6 +8,7 @@
 
 #include "breath/diagnostics/exception.hpp"
 #include "breath/counting/count.hpp"
+#include <algorithm>
 #include <cstddef>
 #include <cstring>
 
@@ -30,9 +31,15 @@ exception::exception( std::string const & what_msg ) noexcept
     char const          sep[] = ": " ;
     std::strcpy( curr, sep ) ;
     curr += ( count( sep ) - 1 ) ;
-    std::strncpy( curr, what_msg.c_str(),
-        static_cast< std::size_t >( what_message_buffer_size -
-                                    ( curr - &m_what_message[ 0 ] ) ) ) ;
+
+    std::size_t const   length_so_far = curr - &m_what_message[ 0 ] ;
+    std::size_t const   remaining_length = what_message_buffer_size - 1 -
+                                            length_so_far ;
+    std::size_t const   length_to_copy = std::min( what_msg.length(),
+                                            remaining_length ) ;
+
+    std::strncpy( curr, what_msg.c_str(), length_to_copy ) ;
+    curr[ length_to_copy ]  = '\0' ;
 }
 
 char const *
