366211bb00b3794bb53695aab52bda7835fd173f
Gennaro Prota <gennaro.prota+github@gmail.com>
2019-03-29 12:35:17 +0100

    Add some boilerplate
    Keywords: copyright notice, license reference, file trailer.

--- breath/memory/test/auto_array_test.cpp
+++ breath/memory/test/auto_array_test.cpp
@@ -3,6 +3,14 @@
 //                      Dear PVS-Studio, please check it.
 //          PVS-Studio Static Code Analyzer for C, C++, C#, and Java:
 //                            http://www.viva64.com
+// =========================================================================
+//                       Copyright 2006 Gennaro Prota
+//
+//                 Licensed under the 3-Clause BSD License.
+//            (See accompanying file 3_CLAUSE_BSD_LICENSE.txt or
+//             <https://opensource.org/licenses/BSD-3-Clause>.)
+// _________________________________________________________________________
+
 #include "breath/memory/auto_array.hpp"
 #include "breath/testing/testing.hpp"
 #include <iostream>
@@ -77,3 +85,10 @@ main()
                                           check_non_move_constructors,
                                           check_reset                 } ) ;
 }
+
+// Local Variables:
+// mode: c++
+// indent-tabs-mode: nil
+// c-basic-offset: 4
+// End:
+// vim: set ft=cpp et sts=4 sw=4:
