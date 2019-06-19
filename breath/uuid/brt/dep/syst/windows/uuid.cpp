ac38c99cc000286ff064e2905329344f8e465969
Gennaro Prota <gennaro.prota+github@gmail.com>
2019-06-19 14:11:26 +0200

    Keep the code within 80 columns
    Other keywords: formatting style, guidelines.

--- breath/uuid/brt/dep/syst/windows/uuid.cpp
+++ breath/uuid/brt/dep/syst/windows/uuid.cpp
@@ -38,10 +38,11 @@ uuid::uuid( uuid::variant var, uuid::version ver )
     //
     FILETIME            ft ;
     ::GetSystemTimeAsFileTime( &ft ) ;
-    std::uint64_t const time_stamp = reinterpret_cast< std::uint64_t const & >( ft ) +
-                                     std::uint64_t( 1000 * 1000 * 10 )
-                                   * std::uint64_t( 60 * 60 * 24 )
-                                   * std::uint64_t( 17 + 30 + 31 + 365 * 18 + 5 ) ;
+    std::uint64_t const time_stamp =
+                              reinterpret_cast< std::uint64_t const & >( ft ) +
+                                 std::uint64_t( 1000 * 1000 * 10 )
+                               * std::uint64_t( 60 * 60 * 24 )
+                               * std::uint64_t( 17 + 30 + 31 + 365 * 18 + 5 ) ;
 
     m_time_low = time_stamp & 0xFFFF'FFFF ;
     m_time_mid = (time_stamp >> 32) & 0xFFFF ;
