2492813a93d4d0d04980ea79c1afc153a3c47cfa
Gennaro Prota <gennaro.prota+github@gmail.com>
2019-06-20 15:15:50 +0200

    Improve some reinterpret_casts in the get_cpuid_info() example

--- breath/cpu/example/cpu.cpp
+++ breath/cpu/example/cpu.cpp
@@ -14,6 +14,7 @@
 #include "breath/cpu/cpuid.hpp"
 #include "breath/diagnostics/assert.hpp"
 
+#include <cstdint>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
@@ -68,9 +69,9 @@ cpu_vendor_id_string()
 
     int const           len = 12 ;
     char                id_string[ len ] ;
-    *reinterpret_cast< int * >( id_string     ) = r.ebx ;
-    *reinterpret_cast< int * >( id_string + 4 ) = r.edx ;
-    *reinterpret_cast< int * >( id_string + 8 ) = r.ecx ;
+    *reinterpret_cast< std::uint32_t * >( id_string     ) = r.ebx ;
+    *reinterpret_cast< std::uint32_t * >( id_string + 4 ) = r.edx ;
+    *reinterpret_cast< std::uint32_t * >( id_string + 8 ) = r.ecx ;
 
     return std::string( id_string, len ) ;
 }
