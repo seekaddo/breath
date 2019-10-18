07d9369063e7888869cfa4eac92b08f19e069dfb
Gennaro Prota <gennaro.prota+github@gmail.com>
2019-10-18 10:57:57 +0200

    Refactor the line_up tool
    Contextually, this better documents the code.

--- tool/line_up/main.cpp
+++ tool/line_up/main.cpp
@@ -38,85 +38,133 @@
 
 namespace {
 
-//      Configuration constants (will be command line arguments).
+//      Configuration constants (will be command line arguments):
+//      =========================================================
+//
+//      Put the normal_line_terminator at column 1 + k * indent_width,
+//      with k being a positive integer (at least one space is added).
 // ---------------------------------------------------------------------------
-bool const          use_indent_width = true ; // put the marker at 1 + k*w, where w
-                                              // is the indentation width (at least
-                                              // one space is added)
+bool const          use_indent_width = true ;
+
 int const           indent_width = 4 ;
 
-// This is made a string for generality, but we really only need a
-// character for now.
-std::string const   marker( "\\" ) ;
+//      This is made a string for generality, but we really only need a
+//      character for now.
+// ---------------------------------------------------------------------------
+std::string const   normal_line_terminator( "\\" ) ;
 
 std::string const   last_line_terminator( "/**/" ) ;
+
+
+//      read_all():
+//      ===========
+//
+//      Reads all lines from 'in', stripping all normal_line_terminators
+//      and spaces that (immediately) precede or follow them. Newlines
+//      are stripped, too. Tab characters are expanded.
+// ---------------------------------------------------------------------------
+std::vector< std::string >
+read_all( std::istream & in )
+{
+    std::vector< std::string >
+                        result ;
+    std::string         line ;
+
+    while ( in.peek() != EOF ) {
+        char const          ch = static_cast< char >( in.get() ) ;
+
+        switch ( ch ) {
+
+        case '\n':
+            line = breath::trim_tail( line ) ;
+            line = breath::remove_from_end( line, normal_line_terminator ) ;
+            line = breath::trim_tail( line ) ;
+
+            result.push_back( line ) ;
+            line.clear() ;
+            break ;
+
+        case '\t':
+            line += std::string( indent_width - line.length() % indent_width,
+                                 ' ' ) ;
+            break ;
+
+        default:
+            line += ch ;
+            break ;
+        }
+    }
+
+    return result ;
+}
+
+//      max_element_length():
+//      =====================
+//
+//      Returns 0 if 'v' is empty; otherwise the length of the longest
+//      string in 'v'.
+// ---------------------------------------------------------------------------
+std::string::size_type
+max_element_length( std::vector< std::string > const & v )
+{
+    auto const          iter =
+            std::max_element( v.cbegin(), v.cend(),
+                              []( std::string const & a, std::string const & b )
+                              {
+                                  return a.length() < b.length() ;
+                              } ) ;
+
+    return iter == v.cend()
+               ? 0
+               : iter->length() ;
+}
 
 }
 
 int
 main()
 {
-    std::istream &      in( std::cin ) ;
-    std::string         line ;
-    std::vector< std::string >
-                        lines ;
     typedef std::string::size_type
                         size_type ;
-    size_type           max_length = 0 ;
 
-    while ( in.peek() != EOF ) {
-
-        char const          ch = static_cast< char >( in.get() ) ;
-        if ( ch == '\n' ) {
-
-            // remove <spaces><marker><spaces>
-            //
-            line = breath::trim_tail( line ) ;
-            line = breath::remove_from_end( line, marker ) ;
-            line = breath::trim_tail( line ) ;
-
-            lines.push_back( line ) ;
-
-            if ( line.length() > max_length ) {
-                max_length = line.length() ;
-            }
-            line.clear() ;
-        } else if ( ch== '\t' ) {
-            line += std::string( indent_width - line.length() % indent_width,
-                                 ' ' ) ;
-        } else {
-            line += ch ;
-        }
-    }
+    std::istream &      in( std::cin ) ;
+    std::vector< std::string> const
+                        lines( read_all( in ) ) ;
 
     BREATH_ASSERT( lines.size() > 1 ) ;
 
-    size_type marker_column =
-      last_line_terminator.length() > marker.length()
-        ? max_length + ( last_line_terminator.length() - marker.length() ) + 2
-        : max_length + 2 ;
+    size_type const     max_length = max_element_length( lines ) ;
 
-    if ( use_indent_width && ( marker_column % indent_width ) != 1 ) {
-        if ( ( marker_column % indent_width ) == 0 ) {
-            marker_column += 1 ;
+    size_type column =
+        last_line_terminator.length() > normal_line_terminator.length()
+            ? max_length + 2 + ( last_line_terminator.length() -
+                                 normal_line_terminator.length() )
+            : max_length + 2 ;
+
+    if ( use_indent_width && ( column % indent_width ) != 1 ) {
+        if ( ( column % indent_width ) == 0 ) {
+            column += 1 ;
         } else {
-            marker_column += ( indent_width - marker_column % indent_width
-                                                                         + 1 ) ;
+            column += ( indent_width - column % indent_width + 1 ) ;
         }
     }
 
-    for ( auto it = lines.begin() ; it != lines.end() ; ++ it ) {
+    for ( auto it = lines.cbegin() ; it != lines.cend() ; ++ it ) {
 
-        size_type           space_count = it != ( lines.end() - 1 )
-                                              ? marker_column - 1 - it->length()
-                                              : marker_column - 1 - it->length()  - last_line_terminator.length() + marker.length() ;
+        bool const          is_last_line = it == lines.cend() - 1 ;
 
+        size_type const     space_count = is_last_line
+                                ? column - 1 - it->length()             -
+                                        last_line_terminator.length()   +
+                                        normal_line_terminator.length()
+                                : column - 1 - it->length() ;
 
         std::string const   spaces( space_count, ' ' ) ;
 
-        std::cout << *it << spaces << ( it == ( lines.end() - 1 )
+        std::cout << *it << spaces << ( is_last_line
                                           ? last_line_terminator
-                                          : marker ) << std::endl ;
+                                          : normal_line_terminator )
+                                                                  << std::endl ;
 
     }
 }
