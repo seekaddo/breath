27e6065e839f13982aad424900b54670fa12d531
Gennaro Prota <gennaro.prota+github@gmail.com>
2019-10-29 16:06:10 +0100

    Be consistent with a link
    This adds "http://" (which may also help your editor recognize it as a
    link), puts the URL on a line of its own, and uses lowercase for 'P' and
    'R', as indicated at <http://wg21.link>.

--- breath/algorithm/secure_fill.hpp
+++ breath/algorithm/secure_fill.hpp
@@ -55,8 +55,11 @@ namespace breath {
 //
 //      Note that, finally (!), the C++17 standard talks about "accesses
 //      through volatile glvalues" [intro.execution] instead of "access
-//      to volatile objects". This stems from <wg21.link/P0612R0>, which
-//      was adopted on March, 2017 (post-Kona mailing). For C, see
+//      to volatile objects". This stems from
+//
+//        <http://wg21.link/p0612r0>,
+//
+//      which was adopted on March, 2017 (post-Kona mailing). For C, see
 //      DR476:
 //
 //        <http://www.open-std.org/jtc1/sc22/wg14/www/docs/summary.htm#dr_476>.
