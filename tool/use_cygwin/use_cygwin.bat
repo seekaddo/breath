d66c1c5eebe878e7862f3048fb5396c219c22351
Gennaro Prota <gennaro.prota+github@gmail.com>
2019-11-21 13:18:33 +0100

    Re-wrap some comments
    Reason: consistency (these comments were probably from when we wrapped
    at a different column).
    
    Other keywords: word-wrapping, aesthetics.

--- tool/use_cygwin/use_cygwin.bat
+++ tool/use_cygwin/use_cygwin.bat
@@ -7,12 +7,11 @@ REM             (See accompanying file 3_CLAUSE_BSD_LICENSE.txt or
 REM              <https://opensource.org/licenses/BSD-3-Clause>.)
 REM __________________________________________________________________________
 REM
-REM     A quick tool to invoke before compiling with Cygwin, on
-REM     Windows.
+REM     A quick tool to invoke before compiling with Cygwin, on Windows.
 REM
-REM     The only important thing that this does is putting /usr/bin
-REM     at the *end* of %PATH%, so that Cygwin's `link' isn't found
-REM     before the homonym Microsoft linker.
+REM     The only important thing that this does is putting /usr/bin at
+REM     the *end* of %PATH%, so that Cygwin's `link' isn't found before
+REM     the homonym Microsoft linker.
 REM
 REM     Usage:
 REM         1. invoke the Visual Studio Command Prompt of your choice
