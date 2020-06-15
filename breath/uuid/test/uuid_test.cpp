c87688740d27611c8cb8b0e191c868d84dd79480
Gennaro Prota <gennaro.prota+github@gmail.com>
2020-06-15 15:09:26 +0200

    Include the uuid tests in the Visual Studio test project
    Reason: somehow we missed them in the previous commits.

--- breath/uuid/test/uuid_test.cpp
+++ breath/uuid/test/uuid_test.cpp
@@ -73,14 +73,16 @@ do_tests()
 
 
 int
-main()
+test_uuid()
 {
     using namespace breath ;
 
     console_reporter    cr( std::cout ) ;
     test_runner::instance().attach_reporter( cr ) ;
 
-    return test_runner::instance().run( { do_tests } ) ;
+    return test_runner::instance().run(
+             "uuid",
+             { do_tests } ) ;
 }
 
 // Local Variables:
