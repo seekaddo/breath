19725b13ce2dfe6dfecde984173ab0f5aaa9fa43
Gennaro Prota <gennaro.prota+github@gmail.com>
2020-06-29 15:47:14 +0200

    Clarify the file-level documentation of representation_in_base.hpp
    Reason: we didn't define 'b', in that context.

--- breath/mathematics/representation_in_base.hpp
+++ breath/mathematics/representation_in_base.hpp
@@ -7,7 +7,7 @@
 // ___________________________________________________________________________
 //
 //!     \file
-//!     \brief The representation of an integer in base \c b.
+//!     \brief The representation of an integer in a given base.
 // ---------------------------------------------------------------------------
 
 #ifndef BREATH_GUARD_lqF1VhyaeQBA6t5SZLvv9rpFKK1iZVzu
